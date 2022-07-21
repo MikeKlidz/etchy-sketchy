4bce1dc546c11c177b0824c98b9de296f8387c7c
Mikhail Klidzhan <johniehuge@gmail.com>
2022-07-21 21:53:17 +0300

    Added buttons for colors and expanded the grid up to 64x64

--- index.js
+++ index.js
@@ -1,15 +1,73 @@
 const cells = document.querySelectorAll('.cell');
 const resetButton = document.querySelector('.reset');
+const colorButtons = document.querySelectorAll('.btn');
+const redButton = document.querySelector('.red');
+const greenButton = document.querySelector('.green');
+const yellowButton = document.querySelector('.yellow');
+const blueButton = document.querySelector('.blue');
+const defaultButton = document.querySelector('.default');
 
 
-cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'black';
-    })
-})
 
-resetButton.addEventListener('click', () => {
+function resetBoard(){
     cells.forEach((cell) => {
         cell.style.backgroundColor = 'white';
     })
-})
+}
+
+function changePenColor() {
+    colorButtons.forEach((button) => {
+        
+        button.addEventListener('click', (e) => {
+            switch(e.target.textContent) {
+                case 'Red':
+                    cells.forEach((cell) => {
+                        cell.addEventListener('mouseover', ()=> {
+                            cell.style.backgroundColor = 'red';
+                        })
+                    })
+                    break;
+                case 'Green':
+                    cells.forEach((cell) => {
+                        cell.addEventListener('mouseover', ()=> {
+                            cell.style.backgroundColor = 'green';
+                        })
+                    })
+                    break;
+                case 'Yellow':
+                    cells.forEach((cell) => {
+                        cell.addEventListener('mouseover', ()=> {
+                            cell.style.backgroundColor = 'yellow';
+                        })
+                    })
+                    break;
+                case 'Blue':
+                    cells.forEach((cell) => {
+                        cell.addEventListener('mouseover', ()=> {
+                            cell.style.backgroundColor = 'blue';
+                        })
+                    })
+                    break;
+                case 'Default':
+                    cells.forEach((cell) => {
+                        cell.addEventListener('mouseover', ()=> {
+                            cell.style.backgroundColor = 'black';
+                        })
+                    })
+                    break;
+                default:
+                    cells.forEach((cell) => {
+                        cell.addEventListener('mouseover', ()=> {
+                            cell.style.backgroundColor = 'black';
+                        })
+                    })
+                    break;
+            }
+
+        })
+    })
+    }
+
+changePenColor();
+
+resetButton.addEventListener('click', resetBoard);
