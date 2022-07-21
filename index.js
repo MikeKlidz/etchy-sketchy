b05799596a90dfb10cfaa468a801a600bb0b4eb9
Mikhail Klidzhan <johniehuge@gmail.com>
2022-07-21 19:34:08 +0300

    Added a reset button and a border for the grid

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const cells = document.querySelectorAll('.cell');
+const resetButton = document.querySelector('.reset');
+
 
 cells.forEach((cell) => {
     cell.addEventListener('mouseover', () => {
         cell.style.backgroundColor = 'black';
+    })
+})
+
+resetButton.addEventListener('click', () => {
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = 'white';
     })
 })
